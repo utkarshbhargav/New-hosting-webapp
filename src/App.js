3779df8500abb846b734bb6b48d40161823f2ee7
Utkarsh <ubhargav14@gmail.com>
2021-12-15 22:27:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+         <form >
+            <div >
+              <p >
+                <input 
+                  type="text"
+                  placeholder="Enter username or email"
+                                 />
+              </p>
+            </div>
+            <div >
+              <p >
+                <input 
+                  type="text"
+                  placeholder="Enter password"
+                                 />
+              </p>
+            </div>
+           
+            <div className="field">
+              <p className="control">
+                <button className="button is-success">
+                  Login
+                </button>
+              </p>
+            </div>
+          </form>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+
