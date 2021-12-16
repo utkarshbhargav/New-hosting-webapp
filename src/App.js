baa076443c94a2928798491aaaf3db5b16d301c0
Utkarsh <ubhargav14@gmail.com>
2021-12-16 15:21:02 +0530

    ic13

--- src/App.js
+++ src/App.js
@@ -1,84 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
 
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
-import ChangePassword from './components/auth/ChangePassword';
-import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
-import Welcome from './components/auth/Welcome';
-import {Auth} from 'aws-amplify';
-
-
-export default class App extends Component {
-// creating a state to authenticate
-  state = 
-  {
-    isAuthenticated: false,
-    isAuthenticating: true,
-    user: null
-  }
-  setAuthStatus = authenticated => 
-  {
-    this.setState({ isAuthenticated: authenticated });
-  }
-  setUser = user => 
-  {
-    this.setState({ user: user });
-  }
-
-// loading session object 
-  async componentDidMount(){
-    try {
-      const session = await Auth.currentSession();
-    this.setAuthStatus(true);
-    console.log(session);
-
-    const user =  await Auth.currentAuthenticatedUser();
-    this.setUser(user);
-    } catch (error) {
-      console.log(error);
-    }
-     this.setState({ isAuthenticating: false });
-  };
-
-
-  render() {
-    const authProps ={
-      isAuthenticated: this.state.isAuthenticated,
-      user: this.state.user,
-      setAuthStatus: this.setAuthStatus,
-      setUser: this.setUser 
-
-
-    }
-    return (
-    !this.state.isAuthenticating && 
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar auth={authProps}/>
-            <Switch>
-              <Route exact path="/" render={(props)=> <Home {...props} auth={authProps}/>} />
-              <Route exact path="/login" render={(props)=> <LogIn {...props} auth={authProps}/>} />
-              <Route exact path="/register" render={(props)=> <Register {...props} auth={authProps}/>} />
-              <Route exact path="/forgotpassword" render={(props)=> <ForgotPassword {...props} auth={authProps}/>} />
-              <Route exact path="/forgotpasswordverification" render={(props)=> <ForgotPasswordVerification {...props} auth={authProps}/>} />
-              <Route exact path="/changepassword" render={(props)=> <ChangePassword {...props} auth={authProps}/>} />
-              <Route exact path="/changepasswordconfirmation" render={(props)=> <ChangePasswordConfirm {...props} auth={authProps}/>} />
-              <Route exact path="/welcome" render={(props)=> <Welcome {...props} auth={authProps}/>} />
-            </Switch>
-         
-          </div>
-        </Router>
-      </div>
-    
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-// export default App;
+export default App;
