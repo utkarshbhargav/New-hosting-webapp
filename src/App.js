be7ffd8e6023786cec00ed7c9ace5c3ee2a6fb8d
Utkarsh <ubhargav14@gmail.com>
2021-12-16 12:12:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Register from './components/auth/Register';
+
 
 import './App.css';
 export default class App extends Component {
   render() {
     return (
       <div>
-         <form >
-            <div >
-              <p >
-                <input 
-                  type="text"
-                  placeholder="Enter username or email"
-                                 />
-              </p>
-            </div>
-            <div >
-              <p >
-                <input 
-                  type="text"
-                  placeholder="Enter password"
-                                 />
-              </p>
-            </div>
-           
-            <div className="field">
-              <p className="control">
-                <button className="button is-success">
-                  Login
-                </button>
-              </p>
-            </div>
-          </form>
-        
+        <Register />
       </div>
     )
   }
