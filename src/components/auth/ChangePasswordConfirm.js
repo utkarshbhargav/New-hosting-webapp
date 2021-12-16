8b25bc348e64530e0753b20e77dd45b45f1e87fa
Utkarsh <ubhargav14@gmail.com>
2021-12-16 12:55:05 +0530

    ic11

---
+++ src/components/auth/ChangePasswordConfirm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ChangePasswordConfirmation extends Component {
+  render() {
+    return (
+       <div>
+          
+          <p>Your password has been successfully updated!</p>
+        </div>
+        );
+     
+  }
+};
+
+export default ChangePasswordConfirmation;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config  from './config' 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+Amplify.configure({
+    Auth:{
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+})
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
