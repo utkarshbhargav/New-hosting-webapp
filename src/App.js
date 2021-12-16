4df5919ed7c7c0306d1b806e067c4722c5b79ce5
Utkarsh <ubhargav14@gmail.com>
2021-12-16 15:38:51 +0530

    ic14

--- src/App.js
+++ src/App.js
@@ -1,81 +1,10 @@
-
+import Register from './Register';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <section className="section auth">
-        <div className="container">
-          <h1>Register</h1>
-      
-            <div className="field">
-              <p className="control">
-                <input 
-                  className="input" 
-                  type="text"
-                  id="username"
-                 placeholder="Enter username"
-                 
-                />
-              </p>
-
-            </div>
-            <div className="field">
-              <p className="control has-icons-left has-icons-right">
-                <input 
-                  className="input" 
-                  type="email"
-                  id="email"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                  
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input 
-                  className="input" 
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-               
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input 
-                  className="input" 
-                  type="password"
-                  id="confirmpassword"
-                  placeholder="Confirm password"
-                
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-              </p>
-            </div>
-            
-            <div className="field">
-              <p className="control">
-                <button className="button is-success">
-                  Register
-                </button>
-              </p>
-              
-            </div>  
-        </div>
-      </section>
-      </header>
+      <Register />
     </div>
   );
 }
